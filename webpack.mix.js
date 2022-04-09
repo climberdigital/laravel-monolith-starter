e367647ea439cf550b9bad089a0d7469d128ceae
Oleg Knyazev <climberdigital@gmail.com>
2022-04-09 13:22:56 +0300

    Fix wrong file references in Mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,14 @@
 const mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
+const path = require('path');
 
 mix.js('resources/js/website/app.js', 'public/dist/website/js') // for vanilla JS
 .js('resources/js/admin/app.js', 'public/dist/admin/js').vue() // for the Vue.js app
 .postCss('resources/css/website/app.css', 'public/dist/website/css', [
-   tailwindcss('tailwindcss.public.config.js'),
+   tailwindcss('tw.website.config.js'),
 ])
 .postCss('resources/css/admin/app.css', 'public/dist/admin/css', [
-   tailwindcss('tailwindcss.public.config.js'),
+   tailwindcss('tw.admin.config.js'),
 ])
 .alias({
     '@': path.join(__dirname, 'resources/js'),
